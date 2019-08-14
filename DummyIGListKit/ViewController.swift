835059443f5ed905b5b4d743f343ccbc961ce445
Edho Prasetyo <edho.prasetyo@tokopedia.com>
2019-08-14 15:02:07 +0700

    first commit

---
+++ DummyIGListKit/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DummyIGListKit
+//
+//  Created by Edho Prasetyo on 14/08/19.
+//  Copyright © 2019 love. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Pods/Target Support Files/Pods-DummyIGListKit/Pods-DummyIGListKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DummyIGListKit : NSObject
+@end
+@implementation PodsDummy_Pods_DummyIGListKit
+@end
